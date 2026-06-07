ca2af69ca83c217e9e608a498e69dcd933b5de0b
learntcm <63395778+learntcm@users.noreply.github.com>
2026-06-07 11:16:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 /* DrivePK Bolo App V2 Configuration */
 window.DRIVEPK_BOLO_CONFIG = {
   APP_NAME: 'DrivePK Bolo',
+  CARS_API_URL: 'https://api.drivepk.com/cars',
   TRANSCRIBE_URL: 'https://meilibeauty.co.uk/transcribe.php',
   VOICE_URL: 'https://meilibeauty.co.uk/voice.php',
   EXPIRY_MINUTES: 10,
   MAX_RECORDING_SECONDS: 75,
-  TRANSCRIPTION_LANGUAGE: 'en',
   STORAGE_KEYS: {
     PROFILE: 'drivepk_bolo_v2_profile',
-    VEHICLES: 'drivepk_bolo_v2_vehicles',
-    GUIDE_PLAYED: 'drivepk_bolo_v2_post_guide_played'
+    VEHICLES: 'drivepk_bolo_v2_vehicles'
   }
 };
