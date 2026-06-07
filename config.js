c9bde2da5b77b805c79a78d6d10e7e80004c8442
learntcm <63395778+learntcm@users.noreply.github.com>
2026-06-07 09:23:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ window.DRIVEPK_BOLO_CONFIG = {
   VOICE_URL: 'https://meilibeauty.co.uk/voice.php',
   EXPIRY_MINUTES: 10,
   MAX_RECORDING_SECONDS: 75,
+  TRANSCRIPTION_LANGUAGE: 'en',
   STORAGE_KEYS: {
     PROFILE: 'drivepk_bolo_v2_profile',
     VEHICLES: 'drivepk_bolo_v2_vehicles',
     GUIDE_PLAYED: 'drivepk_bolo_v2_post_guide_played'
   }
 };
-
